7f7838a3ce4ff0eff7f5600c344a9482ef789138
razzaksr <razzaksr@gmail.com>
2025-01-31 10:24:27 +0530

    FSD without Hibernate done

--- dgct-cse-fsd/src/main/java/crud/cse/dgct_cse_fsd/WebAPI.java
+++ dgct-cse-fsd/src/main/java/crud/cse/dgct_cse_fsd/WebAPI.java
@@ -1,8 +1,12 @@
 package crud.cse.dgct_cse_fsd;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebAPI {
@@ -12,5 +16,31 @@ public class WebAPI {
     @GetMapping("/")
     public String showLand(){
         return "index";
-    }    
+    }
+    // to show register page
+    @GetMapping("/register")
+    public String showRegister(Model model){
+        Patient patient = new Patient();
+        model.addAttribute("newPatient", patient);
+        return "register";
+    }
+    @PostMapping("/new")
+    public String regitering(Patient patient){
+        //System.out.println("What i have received from page "+patient.getPatientName());
+        patientRepository.save(patient);
+        System.out.println(patientRepository.findAll().size());
+        return "index";
+    }
+    @GetMapping("/view")
+    public String view(Model model){
+        List<Patient> temp = patientRepository.findAll();
+        model.addAttribute("total", temp);
+        return "view";
+    }
+    @GetMapping("/obese")
+    public String obese(Model model){
+        List<Patient> temp = patientRepository.findAllByObese();
+        model.addAttribute("total", temp);
+        return "view";
+    }
 }
